cdc66af33ecefeaee770520fe1f43cb636a90b9d
Takahiro Kenmotsu <kenmotsu@techmatrix.co.jp>
2024-11-15 16:32:13 +0900

    first commit

---
+++ src/test/java/examples/calculator/CalcTest.java
@@ -0,0 +1,16 @@
+package examples.calculator;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CalcTest {
+
+    @Test
+    void testAdd() {
+        Calc calc = new Calc();
+        int expected = 2;
+        int actual = calc.add(1, 1);
+        assertEquals(expected, actual);
+    }
+}
