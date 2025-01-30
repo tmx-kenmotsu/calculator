03d1b98ac0ba53b8ea8c36cedf6b025238f4108a
Takahiro Kenmotsu <kenmotsu@techmatrix.co.jp>
2025-01-30 16:40:42 +0900

    test

--- src/main/java/examples/flowanalysis/UncontrolledCommandLine.java
+++ src/main/java/examples/flowanalysis/UncontrolledCommandLine.java
@@ -4,9 +4,8 @@ import java.io.IOException;
 
 public class UncontrolledCommandLine {
 
-    //public void test() throws IOException {
     public static void main(String[] args) throws IOException {
-        String script = System.getenv("SCRIPTNAME");
+        String script = System.getenv(args[0]);
         if (script != null) {
             // BAD: The script to be executed is controlled by the user.
             Runtime.getRuntime().exec(script);
